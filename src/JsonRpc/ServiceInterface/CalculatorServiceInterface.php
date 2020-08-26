3fcf8516332b57a7790745e66fa7eac0025301f0
chen <chenchangze@youanmi.com>
2020-08-26 16:51:13 +0800

    1

--- src/JsonRpc/ServiceInterface/CalculatorServiceInterface.php
+++ src/JsonRpc/ServiceInterface/CalculatorServiceInterface.php
@@ -8,4 +8,5 @@ interface CalculatorServiceInterface
     public function add(int $a, int $b): int;
     public function mul(int $a, int $b): int;
     public function sub(int $a, int $b): int;
+	public function login(string $username, String $passwoed);
 }
